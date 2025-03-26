90472880e20090185226800867e3282df5980aa5
Hemamalini-L <hemamalini291204@gmail.com>
2025-03-26 22:07:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-!pip install PyPDF
 
 !pip install streamlit
 
